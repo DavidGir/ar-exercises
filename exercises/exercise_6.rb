d683e09bf154611784821425d30ebab8d9f7da28
David Giroux <DavidGiroux149@gmail.com>
2023-12-12 15:29:56 -0500

    create employees data for store 1 and 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Alice", last_name: "Smith", hourly_rate: 50)
+@store1.employees.create(first_name: "Bob", last_name: "Johnson", hourly_rate: 55)
+
+# Employees for @store2
+@store2.employees.create(first_name: "Carol", last_name: "Brown", hourly_rate: 58)
+@store2.employees.create(first_name: "Dave", last_name: "Wilson", hourly_rate: 53)
+
+pp @store1.employees
+pp @store2.employees
