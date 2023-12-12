0f8f715cfba2b55787b2792579a478c6214b38f0
David Giroux <DavidGiroux149@gmail.com>
2023-12-12 16:02:51 -0500

    add retrieve user input, save and display errors

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,18 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+# Ask the user for a store name
+puts "Please enter a store name: "
+# Retrieve input name 
+store_name = gets.chomp 
+# Create object instantiation
+new_store = Store.new(name: store_name)
+
+if new_store.save
+  puts "Store created successfully!"
+else
+  puts "Error: Could not create store."
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+end
